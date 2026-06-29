a761be5434014c5e74abb0606c233a251dcd95c7
nfforbes <nfforbes@gmail.com>
2026-06-29 13:35:22 -0400

    fix platform time for build

--- apps/kt/shared/src/iosMain/kotlin/com/weirhere/data/PlatformTime.ios.kt
+++ apps/kt/shared/src/iosMain/kotlin/com/weirhere/data/PlatformTime.ios.kt
@@ -1,10 +1,18 @@
 package com.weirhere.data
 
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
 import platform.Foundation.NSDate
 import platform.Foundation.NSDateFormatter
+import platform.posix.gettimeofday
+import platform.posix.timeval
 
-internal actual fun epochMillis(): Long =
-    (NSDate().timeIntervalSince1970() * 1000.0).toLong()
+internal actual fun epochMillis(): Long = memScoped {
+    val timeVal = alloc<timeval>()
+    gettimeofday(timeVal.ptr, null)
+    timeVal.tv_sec * 1000L + timeVal.tv_usec / 1000L
+}
 
 internal actual fun currentMonthYyyyMm(): String {
     val formatter = NSDateFormatter()
